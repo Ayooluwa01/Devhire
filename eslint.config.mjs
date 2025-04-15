82a17bdd3fa41a28cf9ec475bebcbddfc3ffc7cc
OLUSEGUN STEPHEN AYOOLUWA <olusegunstephen222@gmail.com>
2025-04-15 09:27:30 +0100

    Added more rules to eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -33,6 +33,7 @@ const eslintConfig = [
     files: ["**/*.ts", "**/*.tsx"],
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "off",
     },
   },
 ];
