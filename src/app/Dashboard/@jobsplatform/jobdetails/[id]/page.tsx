7993b927a627baa464200f90d329eff0aeaf5a04
OLUSEGUN STEPHEN AYOOLUWA <olusegunstephen222@gmail.com>
2025-04-18 08:16:04 +0100

    dbg 10

--- src/app/Dashboard/@jobsplatform/jobdetails/[id]/page.tsx
+++ src/app/Dashboard/@jobsplatform/jobdetails/[id]/page.tsx
@@ -6,23 +6,5 @@ export default async function Editjobs(props: {
     id: number;
   }>;
 }) {
-  // const headersList = headers();
-  // const protocol = (await headersList).get("x-forwarded-proto") || "http";
-  // const host = (await headersList).get("host"); // Fallback for local dev
-  const id = (await props.params).id;
-  // const baseUrl = `${protocol}://${host}`;
-  // const res = await fetch(
-  //   `https://devhiretalents.vercel.app/api/joblisting/${
-  //     (
-  //       await props.params
-  //     ).id
-  //   }`
-  // );
-
-  // if (!res.ok) {
-  //   return <div className="text-center text-red-500">Job not found</div>;
-  // }
-
-  // const details = await res.json();
-  return <>{id}</>;
+  return <div>Hello,{(await props.params).id}</div>;
 }
