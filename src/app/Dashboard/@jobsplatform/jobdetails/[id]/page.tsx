37b5147a714f1bb86b4170fd25c14f1ae56bd009
OLUSEGUN STEPHEN AYOOLUWA <olusegunstephen222@gmail.com>
2025-04-18 08:20:31 +0100

    dbg 11

--- src/app/Dashboard/@jobsplatform/jobdetails/[id]/page.tsx
+++ src/app/Dashboard/@jobsplatform/jobdetails/[id]/page.tsx
@@ -1,10 +1,17 @@
+import axios from "axios";
 import JobDetailsClient from "@/Components/JobDetailsClient";
-import { headers } from "next/headers";
 
-export default async function Editjobs(props: {
-  params: Promise<{
-    id: number;
-  }>;
-}) {
-  return <div>Hello,{(await props.params).id}</div>;
+export default async function Editjobs({ params }: { params: { id: number } }) {
+  try {
+    const res = await axios.get(
+      `https://devhiretalents.vercel.app/api/joblisting/${params.id}`
+    );
+
+    const details = res.data;
+
+    return <JobDetailsClient details={details} jobId={params.id} />;
+  } catch (error) {
+    console.error("Failed to fetch job details:", error);
+    return <div className="text-center text-red-500">Job not found</div>;
+  }
 }
