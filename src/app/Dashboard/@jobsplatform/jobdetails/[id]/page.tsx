b6ef44c56346816d50dc3c7a158a6207ee729ceb
OLUSEGUN STEPHEN AYOOLUWA <olusegunstephen222@gmail.com>
2025-04-18 07:40:34 +0100

    attmpt

--- src/app/Dashboard/@jobsplatform/jobdetails/[id]/page.tsx
+++ src/app/Dashboard/@jobsplatform/jobdetails/[id]/page.tsx
@@ -1,24 +1,3 @@
-// import JobDetailsClient from "@/Components/JobDetailsClient";
-// import { headers } from "next/headers";
-// export default async function Jobdetails(props: {
-//   params: Promise<{ id: string }>;
-// }) {
-//   const headersList = headers();
-//   const protocol = (await headersList).get("x-forwarded-proto") || "http";
-//   const host = (await headersList).get("host"); // Fallback for local dev
-
-//   const baseUrl = `${protocol}://${host}`;
-//   const params = await props.params;
-//   const res = await fetch(`${baseUrl}/api/joblisting/${params.id}`);
-
-//   if (!res.ok) {
-//     return <div className="text-center text-red-500">Job not found</div>;
-//   }
-
-//   const details = await res.json();
-
-//   return <JobDetailsClient details={details} jobId={params.id} />;
-// }
 import JobDetailsClient from "@/Components/JobDetailsClient";
 import { headers } from "next/headers";
 
@@ -27,20 +6,5 @@ export default async function Editjobs(props: {
     id: number;
   }>;
 }) {
-  const headersList = headers();
-  const protocol = (await headersList).get("x-forwarded-proto") || "http";
-  const host = (await headersList).get("host"); // Fallback for local dev
-
-  const baseUrl = `${protocol}://${host}`;
-  const params = await props.params;
-  const res = await fetch(`${baseUrl}/api/joblisting/${params.id}`, {
-    cache: "no-store", // Prevents stale data in dynamic route);
-  });
-  if (!res.ok) {
-    return <div className="text-center text-red-500">Job not found</div>;
-  }
-
-  const details = await res.json();
-
-  return <JobDetailsClient details={details} jobId={params.id} />;
+  return <div>Hello</div>;
 }
