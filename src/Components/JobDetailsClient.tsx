1d9c1185b4b8d814888070ea0ec6cc5e414e6602
OLUSEGUN STEPHEN AYOOLUWA <olusegunstephen222@gmail.com>
2025-04-21 10:20:38 +0100

    changed padding to 4

--- src/Components/JobDetailsClient.tsx
+++ src/Components/JobDetailsClient.tsx
@@ -27,7 +27,7 @@ export default function JobDetailsClient({ details, jobId }: any) {
     setapplied(true);
   };
   return (
-    <div className="container mx-auto p-6">
+    <div className="container mx-auto p-4">
       <div className="bg-white rounded-lg p-6 mt-6">
         <p className="text-lg text-gray-600 mt-1 flex flex-row">
           <img
